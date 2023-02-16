ae1df32182ac5ca7a782c19b8bd45656b45aa54a
Dharmendra kumar <dharmendragangwar609@gmail.com>
2023-02-16 12:08:19 +0530

    Its Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
-  
+    public static void main(String[] args)
+    {
+        RWOnly obj=new RWOnly();
+        obj.setName("Dharmendra");
+        obj.getName();
+    }
 }
